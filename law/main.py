e0a54a25f15812ddf413cc8fbc253a9a0e53203d
Stefan Bieliauskas <sbieliauskas@rightmart.de>
2018-11-25 21:21:41 +0200

    Pass argument down to service

--- law/main.py
+++ law/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 driver = GraphDatabase.driver(os.getenv('NEO4J_URL', "bolt://localhost:7687"),
                         auth=(os.getenv("NEO4J_USER", "neo4j"), os.getenv("NEO4J_PASSWORD", "neo4jneo4j")))
-service = LawService()
+service = LawService(driver)
 
 @app.route('/api')
 def api():
