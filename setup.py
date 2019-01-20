cfb33e8dbd1ec1ada5baa6bb7aaf2fd85c2fe5a9
Stefan Bieliauskas <B-Stefan@users.noreply.github.com>
2019-01-21 00:03:25 +0100

    Add restful version 0.3.7

--- setup.py
+++ setup.py
@@ -36,7 +36,8 @@ setup(
     install_requires=[
         'Flask==1.0.2',
         'Scrapy==1.5.1',
-        'neo4j==1.7.1'
+        'neo4j==1.7.1',
+        'flask-restful==0.3.7'
     ],
     extras_require={
         'test': tests_require,
